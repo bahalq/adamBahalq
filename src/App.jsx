cffeaf34728a4c16177cfbaf655099b74e0edf99
Bahalq <bahalqadam@gmail.com>
2026-03-11 10:18:30 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 import About from "./components/About";
 import { useTranslation } from "react-i18next";
 import Projects from "./components/Projects";
-import { useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 function App() {
   const { i18n } = useTranslation();
